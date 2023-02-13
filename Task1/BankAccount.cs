38c825e1319306818bc48b43ee24b3d7c4b90aac
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-13 22:28:27 +0300

    Добавьте файлы проекта.

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class BankAccount<T>
+    {
+        public T Num { get; set; }
+        public int Balance { get; set; }
+        public string Fullname { get; set; }
+        public string GetInfo()
+        {
+            return ($"{Num} {Balance} {Fullname}");
+        }
+        public void Imput()
+        {
+            Console.WriteLine("Введите номер счёта");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца");
+            Fullname = Console.ReadLine();
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Imput();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Imput();
+            Console.WriteLine(bankAccount2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
